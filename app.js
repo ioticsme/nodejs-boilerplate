75c31fb0cadc4b9ba5f26eaab659945b641f44e3
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2022-01-05 21:48:23 +0400

    general middleware

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ require('dotenv').config()
 
 const sequelize = require('./db/dbconnection')
 
-// Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// General Middlewares
+app.use(express.json({ limit: '10mb', type: 'application/vnd.api+json' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.all('*', function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token");
+    next();
+});
 
 // Application health routes
 app.get('/health', async (req, res) => {
